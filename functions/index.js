9cac2c97e0e42b3bf73b92160d08d491727e35f6
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2018-07-21 22:58:13 +0800

    Add MultiCast Message

--- functions/index.js
+++ functions/index.js
@@ -50,7 +50,7 @@ const push = (res, msg) => {
 		uri: `${LINE_MESSAGING_API}/push`,
 		headers: LINE_HEADER,
 		body: JSON.stringify({
-			to: `zzzzz`,
+			to: `U3c28a70ed7c5e7ce2c9a7597632.....`,
 			messages: [
 				{
 					type: `text`,
@@ -62,5 +62,38 @@ const push = (res, msg) => {
 		return res.status(200).send(`Done`);
 	}).catch((error) => {
 		return Promise.reject(error);
+	});
+}
+
+exports.LineBotMulticast = functions.https.onRequest((req, res) => {
+	const text = req.query.text;
+	if (text !== undefined && text.trim() !== ``) {
+		return multicast(res, text);
+	} else {
+		const ret = { message: 'Text not found' };
+		return res.status(400).send(ret);
+	}
+});
+
+const multicast = (res, msg) => {
+	return request({
+		method: `POST`,
+		uri: `${LINE_MESSAGING_API}/multicast`,
+		headers: LINE_HEADER,
+		body: JSON.stringify({
+			to: [`U3c28a70ed7c5e7ce2c9a7597632.....`, `Ua0e8dd654eeb56790bc0e342bfd.....`],
+			messages: [
+				{
+					type: `text`,
+					text: msg
+				}
+			]
+		})
+	}).then(() => {
+		const ret = { message: 'Done' };
+		return res.status(200).send(ret);
+	}).catch((error) => {
+		const ret = { message: `Sending error: ${error}` };
+		return res.status(500).send(ret);
 	});
 }
