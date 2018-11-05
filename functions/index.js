a75e300ffbb33ecd6109365702139dbe382f99c2
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2018-11-05 10:05:06 +0700

    Update dependencies and modify some configuration
    - Enable Node 8
    - Enable ecmaVersion 8

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,95 @@ const request = require('request-promise');
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
 const LINE_HEADER = {
 	'Content-Type': 'application/json',
-	'Authorization': `Bearer xxxxx`
+	'Authorization': `Bearer <YOUR-ACCESS-TOKEN>`
 };
 
-exports.LineBotReply = functions.https.onRequest((req, res) => {
+const runtimeOpts = {
+	timeoutSeconds: 300,
+	memory: '2GB'
+}
+
+exports.LineBotQuickReply = functions.region('asia-northeast1').runWith(runtimeOpts).https.onRequest((req, res) => {
+	if (req.body.events[0].message.type !== 'text') {
+		return;
+	}
+	quickReply(req.body.events[0]);
+});
+
+const quickReply = (event) => {
+	return request({
+		method: `POST`,
+		uri: `${LINE_MESSAGING_API}/reply`,
+		headers: LINE_HEADER,
+		body: JSON.stringify({
+			replyToken: event.replyToken,
+			messages: [
+				{
+					type: `text`,
+					text: event.message.text,
+					quickReply: {
+						items: [
+							{
+								type: `action`,
+								action: {
+									type: `cameraRoll`,
+									label: `Camera Roll`
+								}
+							},
+							{
+								type: `action`,
+								action: {
+									type: `camera`,
+									label: `Camera`
+								}
+							},
+							{
+								type: `action`,
+								action: {
+									type: `location`,
+									label: `Location`
+								}
+							},
+							{
+								type: `action`,
+								imageUrl: `https://cdn1.iconfinder.com/data/icons/mix-color-3/502/Untitled-1-512.png`,
+								action: {
+									type: `message`,
+									label: `Message`,
+									text: `Hello World!`
+								}
+						  	},
+							{
+								type: `action`,
+								action: {
+									type: `postback`,
+									label: `Postback`,
+									data: `action=buy&itemid=123`,
+									displayText: `Buy`
+								}
+						  	},
+							{
+								type: `action`,
+								imageUrl: `https://icla.org/wp-content/uploads/2018/02/blue-calendar-icon.png`,
+								action: {
+									type: `datetimepicker`,
+									label: `Datetime Picker`,
+									data: `storeId=12345`,
+									mode: `datetime`,
+									initial: `2018-08-10t00:00`,
+									max: `2018-12-31t23:59`,
+									min: `2018-08-01t00:00`
+								}
+						  	}
+						]
+					}
+				}
+			]
+		})
+	});
+};
+
+exports.LineBotReply = functions.region('asia-northeast1').runWith(runtimeOpts).https.onRequest((req, res) => {
 	if (req.body.events[0].message.type !== 'text') {
 		return;
 	}
@@ -31,10 +116,10 @@ const reply = (bodyResponse) => {
 	});
 };
 
-exports.LineBotPush = functions.https.onRequest((req, res) => {
+exports.LineBotPush = functions.region('asia-northeast1').runWith(runtimeOpts).https.onRequest((req, res) => {
 	return request({
 		method: `GET`,
-		uri: `https://api.openweathermap.org/data/2.5/weather?appid=yyyyy&units=metric&type=accurate&zip=10330,th`,
+		uri: `https://api.openweathermap.org/data/2.5/weather?appid=<YOUR-APP-ID>&units=metric&type=accurate&zip=10330,th`,
 		json: true
 	}).then((response) => {
 		const message = `City: ${response.name}\nWeather: ${response.weather[0].description}\nTemperature: ${response.main.temp}`;
@@ -50,7 +135,7 @@ const push = (res, msg) => {
 		uri: `${LINE_MESSAGING_API}/push`,
 		headers: LINE_HEADER,
 		body: JSON.stringify({
-			to: `zzzzz`,
+			to: `<YOUR-UID>`,
 			messages: [
 				{
 					type: `text`,
@@ -64,3 +149,70 @@ const push = (res, msg) => {
 		return Promise.reject(error);
 	});
 }
+
+exports.LineBotMulticast = functions.region('asia-northeast1').runWith(runtimeOpts).https.onRequest((req, res) => {
+	const text = req.query.text;
+	if (text !== undefined && text.trim() !== ``) {
+		return multicast(res, text);
+	} else {
+		const ret = { message: `Text not found` };
+		return res.status(400).send(ret);
+	}
+});
+
+const multicast = (res, msg) => {
+	return request({
+		method: `POST`,
+		uri: `${LINE_MESSAGING_API}/multicast`,
+		headers: LINE_HEADER,
+		body: JSON.stringify({
+			to: [`<YOUR-UID-1>`, `<YOUR-UID-2>`],
+			messages: [
+				{
+					type: `text`,
+					text: msg
+				}
+			]
+		})
+	}).then(() => {
+		const ret = { message: `Done` };
+		return res.status(200).send(ret);
+	}).catch((error) => {
+		const ret = { message: `Sending error: ${error}` };
+		return res.status(500).send(ret);
+	});
+}
+
+exports.LineBotActions = functions.region('asia-northeast1').runWith(runtimeOpts).https.onRequest((req, res) => {
+	actions(req.body.events[0]);
+});
+
+const actions = (event) => {
+	return request({
+		method: `POST`,
+		uri: `${LINE_MESSAGING_API}/reply`,
+		headers: LINE_HEADER,
+		body: JSON.stringify({
+			replyToken: event.replyToken,
+			messages: [
+				{
+					type: `text`,
+					text: JSON.stringify(event),
+					quickReply: {
+						items: [
+							{
+								type: `action`,
+								action: {
+									type: `postback`,
+									label: `Postback`,
+									data: `action=buy&itemid=123`,
+									displayText: `This message was posted by Postback`
+								}
+						  	}
+						]
+					}
+				}
+			]
+		})
+	});
+};
