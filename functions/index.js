e36307ca7655cba5a8abd6c00ac034582efcc491
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2019-06-07 10:36:32 +0700

    Use async/await and update dependencies
    - ecmaVersion: 2017
    - firebase-admin: 8.0.0
    - firebase-functions: 2.3.1

--- functions/index.js
+++ functions/index.js
@@ -1,163 +1,93 @@
 const functions = require("firebase-functions");
 const request = require("request-promise");
-
+const runtimeOpts = { timeoutSeconds: 4, memory: "2GB" };
+const REGION = "asia-east2";
 const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
-const LINE_UID = "<YOUR-USER-ID>";
+const LINE_UID = "YOUR-LINE-USER-ID";
 const LINE_HEADER = {
   "Content-Type": "application/json",
-  Authorization:
-    "Bearer <YOUR-CHANNEL-ACCESS-TOKEN>"
+  Authorization: "Bearer YOUR-CHANNEL-ACCESS-TOKEN"
 };
+const OPENWEATHER_API = "https://api.openweathermap.org/data/2.5/weather/";
+const OPENWEATHER_APPID = "YOUR-OPENWEATHER-APPID";
 
-const runtimeOpts = {
-  timeoutSeconds: 4,
-  memory: "2GB"
-};
-
-exports.LineBotReply = functions
-  .region("asia-northeast1")
-  .runWith(runtimeOpts)
-  .https.onRequest((req, res) => {
-    if (req.method === "POST") {
-      reply(req.body);
-    }
-    return res.status(200).send(req.method);
-  });
+exports.LineBotReply = functions.region(REGION).runWith(runtimeOpts).https.onRequest((req, res) => {
+  if (req.method === "POST") {
+    reply(req.body);
+  }
+  return res.status(200).send(req.method);
+});
 
 const reply = bodyResponse => {
-  return request({
-    method: "POST",
+  return request.post({
     uri: `${LINE_MESSAGING_API}/reply`,
     headers: LINE_HEADER,
     body: JSON.stringify({
       replyToken: bodyResponse.events[0].replyToken,
-      messages: [
-        {
-          type: "text",
-          text: JSON.stringify(bodyResponse)
-        }
-      ]
+      messages: [{ type: "text", text: JSON.stringify(bodyResponse) }]
     })
   });
 };
 
-exports.LineBotPush = functions
-  .region("asia-northeast1")
-  .runWith(runtimeOpts)
-  .https.onRequest((req, res) => {
-    return request({
-      method: "GET",
-      uri: `https://api.openweathermap.org/data/2.5/weather?appid=<YOUR-APP-ID>&units=metric&type=accurate&zip=10330,th`,
-      json: true
-    })
-      .then(response => {
-        const message = `City: ${response.name}\nWeather: ${
-          response.weather[0].description
-        }\nTemperature: ${response.main.temp}`;
-        return push(res, message);
-      })
-      .catch(error => {
-        return res.status(500).send(error);
-      });
+exports.LineBotPush = functions.region(REGION).runWith(runtimeOpts).https.onRequest(async (req, res) => {
+  let response = await request.get({
+    uri: `${OPENWEATHER_API}?appid=${OPENWEATHER_APPID}&units=metric&type=accurate&zip=10330,th`,
+    json: true
   });
+  const message = `City: ${response.name}\nWeather: ${response.weather[0].description}\nTemperature: ${response.main.temp}`;
+  return push(res, message);
+});
 
-const push = (res, msg) => {
-  return request({
-    method: "POST",
+const push = async (res, msg) => {
+  await request.post({
     uri: `${LINE_MESSAGING_API}/push`,
     headers: LINE_HEADER,
     body: JSON.stringify({
       to: LINE_UID,
-      messages: [
-        {
-          type: "text",
-          text: msg
-        }
-      ]
+      messages: [{ type: "text", text: msg }]
     })
-  })
-    .then(() => {
-      return res.status(200).send("Done");
-    })
-    .catch(error => {
-      return Promise.reject(error);
-    });
+  });
+  return res.status(200).send({ message: `Push: ${msg}` });
 };
 
-exports.LineBotMulticast = functions
-  .region("asia-northeast1")
-  .runWith(runtimeOpts)
-  .https.onRequest((req, res) => {
-    const text = req.query.text;
-    if (text !== undefined && text.trim() !== "") {
-      return multicast(res, text);
-    } else {
-      const ret = { message: "Text not found" };
-      return res.status(400).send(ret);
-    }
-  });
+exports.LineBotMulticast = functions.region(REGION).runWith(runtimeOpts).https.onRequest((req, res) => {
+  const text = req.query.text;
+  if (text !== undefined && text.trim() !== "") {
+    return multicast(res, text);
+  } else {
+    return res.status(400).send({ message: "Text not found" });
+  }
+});
 
-const multicast = (res, msg) => {
-  return request({
-    method: "POST",
+const multicast = async (res, msg) => {
+  await request.post({
     uri: `${LINE_MESSAGING_API}/multicast`,
     headers: LINE_HEADER,
     body: JSON.stringify({
-      to: [
-        LINE_UID,
-        "<ANOTHER-USER-ID>"
-      ],
-      messages: [
-        {
-          type: "text",
-          text: msg
-        }
-      ]
+      to: [LINE_UID, "ANOTHER-LINE-USER-ID"],
+      messages: [{ type: "text", text: msg }]
     })
-  })
-    .then(() => {
-      const ret = { message: "Multicast done" };
-      return res.status(200).send(ret);
-    })
-    .catch(error => {
-      const ret = { message: `Multicast error: ${error}` };
-      return res.status(500).send(ret);
-    });
+  });
+  return res.status(200).send({ message: `Multicast: ${msg}` });
 };
 
-exports.LineBotBroadcast = functions
-  .region("asia-northeast1")
-  .runWith(runtimeOpts)
-  .https.onRequest((req, res) => {
-    const text = req.query.text;
-    if (text !== undefined && text.trim() !== "") {
-      return broadcast(res, text);
-    } else {
-      const ret = { message: "Text not found" };
-      return res.status(400).send(ret);
-    }
-  });
+exports.LineBotBroadcast = functions.region(REGION).runWith(runtimeOpts).https.onRequest((req, res) => {
+  const text = req.query.text;
+  if (text !== undefined && text.trim() !== "") {
+    return broadcast(res, text);
+  } else {
+    const ret = { message: "Text not found" };
+    return res.status(400).send(ret);
+  }
+});
 
-const broadcast = (res, msg) => {
-  return request({
-    method: "POST",
+const broadcast = async (res, msg) => {
+  await request.post({
     uri: `${LINE_MESSAGING_API}/broadcast`,
     headers: LINE_HEADER,
     body: JSON.stringify({
-      messages: [
-        {
-          type: "text",
-          text: msg
-        }
-      ]
+      messages: [{ type: "text", text: msg }]
     })
-  })
-    .then(() => {
-      const ret = { message: "Broadcast done" };
-      return res.status(200).send(ret);
-    })
-    .catch(error => {
-      const ret = { message: `Broadcast error: ${error}` };
-      return res.status(500).send(ret);
-    });
+  });
+  return res.status(200).send({ message: `Broadcast: ${msg}` });
 };
