baa8fefce113668b606a1ddc10adbfe968c60bf8
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2020-02-17 12:09:26 +0700

    Update dependencies

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,17 @@ const request = require("request-promise");
 const runtimeOpts = { timeoutSeconds: 4, memory: "2GB" };
 const REGION = "asia-east2";
 const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
-const LINE_UID = "YOUR-LINE-USER-ID";
+const LINE_CHANNEL_SECRET = "YOUR-LINE-CHANNEL-SECRET";
+const LINE_USER_ID = "YOUR-LINE-USER-ID";
 const LINE_HEADER = {
   "Content-Type": "application/json",
-  Authorization: "Bearer YOUR-CHANNEL-ACCESS-TOKEN"
+  Authorization: "Bearer YOUR-LINE-CHANNEL-ACCESS-TOKEN"
 };
+const DIALOGFLOW_AGENT_ID = "YOUR-DIALOGFLOW-AGENT-ID";
 const OPENWEATHER_API = "https://api.openweathermap.org/data/2.5/weather/";
-const OPENWEATHER_APPID = "YOUR-OPENWEATHER-APPID";
+const OPENWEATHER_APPID = "YOUR-OPEN-WEATHER-APP-ID";
+
+const crypto = require('crypto');
 
 exports.LineBotReply = functions.region(REGION).runWith(runtimeOpts).https.onRequest((req, res) => {
   if (req.method === "POST") {
@@ -64,7 +68,7 @@ const multicast = async (res, msg) => {
     uri: `${LINE_MESSAGING_API}/multicast`,
     headers: LINE_HEADER,
     body: JSON.stringify({
-      to: [LINE_UID, "ANOTHER-LINE-USER-ID"],
+      to: [LINE_USER_ID, "Ua0e8dd654eeb56790bc0e342bfd46e1c"],
       messages: [{ type: "text", text: msg }]
     })
   });
@@ -90,4 +94,42 @@ const broadcast = async (res, msg) => {
     })
   });
   return res.status(200).send({ message: `Broadcast: ${msg}` });
+};
+
+exports.LineProxy = functions.region(REGION).runWith(runtimeOpts).https.onRequest((req, res) => {
+  if (req.method === "POST") {
+    console.info("Header Payload", JSON.stringify(req.headers));
+    console.info("Body Payload", JSON.stringify(req.body));
+
+    const text = JSON.stringify(req.body);
+    const signature = crypto.createHmac('SHA256', LINE_CHANNEL_SECRET).update(text).digest('base64').toString();
+    if (signature !== req.headers['x-line-signature']) {
+      return res.status(401).send('Unauthorized');
+    }
+
+    let event = req.body.events[0]
+    if (event.message !== undefined) {
+      if (event.message.type !== "text") {
+        reply(req.body);
+      } else {
+        postToDialogflow(req);
+      }
+    }
+  }
+  return res.status(200).send(req.method);
+});
+
+const postToDialogflow = payloadRequest => {
+  //payloadRequest.headers.host = "bots.dialogflow.com";
+   payloadRequest.headers.host = "dialogflow.cloud.google.com";
+  return request.post({
+    //uri: `https://bots.dialogflow.com/line/${DIALOGFLOW_AGENT_ID}/webhook`,
+    uri: `https://dialogflow.cloud.google.com/v1/integrations/line/webhook/${DIALOGFLOW_AGENT_ID}`,
+    //headers: {
+    //"x-line-signature": payloadRequest.headers["x-line-signature"],
+    //"content-type": "application/json;charset=UTF-8"
+    //},
+    headers: payloadRequest.headers,
+    body: JSON.stringify(payloadRequest.body)
+  });
 };
